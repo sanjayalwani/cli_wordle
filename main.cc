d530ab81f88db07f0a0bfc155c48b97a58eade6c
Sanjay Alwani <priv.sanjay@gmail.com>
2023-05-01 18:02:25 -0700

    Add input trimming and refactor code

--- main.cc
+++ main.cc
@@ -5,8 +5,10 @@
 #include <sstream>
 #include <vector>
 #include <random>
+#include <algorithm> 
+#include <cctype>
+#include <locale>
 
-// #include "db_wrapper.hh"
 #include "db_structs.hh"
 #include "include/sqlite_orm.h"
 
@@ -15,11 +17,57 @@
 using std::chrono::duration_cast;
 using namespace sqlite_orm;
 
-std::string GREEN_BG  = "\033[37;42m";
-std::string YELLOW_BG = "\033[30;43m";
-std::string WHITE_BG  = "\033[30;47m";
-std::string RESET_BG  = "\033[0m";
 
+auto storage = make_storage(DB_NAME,
+    make_table("Wordles",
+        make_column("word", &Wordles::word, primary_key()),
+        make_column("wasUsed", &Wordles::wasUsed)
+    ),
+    make_table("WordleHistory",
+        make_column("startOfUseUnix", &WordleHistory::startOfUseUnix, primary_key()),
+        make_column("word", &WordleHistory::word)
+    )
+);
+
+/* Game Parameters */
+const uint32_t NUM_TURNS = 6;
+
+/* ANSI Background Color Codes */
+const std::string GREEN_BG  = "\033[37;42m";
+const std::string YELLOW_BG = "\033[30;43m";
+const std::string WHITE_BG  = "\033[30;47m";
+const std::string RESET_BG  = "\033[0m";
+
+/* String handling functions */
+
+// trim functions source: https://stackoverflow.com/a/217605, Evan Teran
+
+// trim from start (in place)
+static inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+}
+
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
+
+// trim from both ends (in place)
+static inline void trim(std::string &s) {
+    rtrim(s);
+    ltrim(s);
+}
+
+std::string stringSanitize(std::string s) {
+    trim(s);
+    return s;
+}
+
+/* Game Functions */
 bool wordleInvalid(std::string guess) {
     return guess.size() != 5;
 }
@@ -67,19 +115,45 @@ std::stringstream getColouring(std::string& guess, std::string& actual) {
     return colourings;
 }
 
+/* Game Loop */
+void game_loop(std::string word_of_day) {
+    uint32_t turns = 0;
+
+    while (turns < NUM_TURNS) {
+        std::cout << "Wordle: ";
+
+        std::string input;
+        std::cin >> input;
+
+        input = stringSanitize(input);
+
+        if (input == "q") return;
+
+        if (wordleInvalid(input)) {
+            std::cout << "That wasn't five letters...\n";
+            continue;
+        }
+
+        int word_exists = storage.get_all<Wordles>(where(c(&Wordles::word) == input)).size();
+        if (!word_exists) {
+            std::cout << "That word is not in our dictionary\n";
+            continue;
+        }
+
+        std::stringstream colouring = getColouring(input, word_of_day);
+        std::cout << colouring.str() << std::endl;
+        if (input == word_of_day) {
+            std::cout << "🎉🎉🎉 in " << turns + 1 << " turns!\n";
+            break;
+        }
+        ++turns;
+    }
+    if (turns == NUM_TURNS) std::cout << "The word was " << word_of_day << std::endl;
+}
+
 int main(void) {
     const auto current_unix_time = std::chrono::system_clock::now();
 
-    auto storage = make_storage(DB_NAME,
-        make_table("Wordles",
-            make_column("word", &Wordles::word, primary_key()),
-            make_column("wasUsed", &Wordles::wasUsed)
-        ),
-        make_table("WordleHistory",
-            make_column("startOfUseUnix", &WordleHistory::startOfUseUnix, primary_key()),
-            make_column("word", &WordleHistory::word)
-        )
-    );
     // Get remaining wordle count
     int remaining_words = storage.count<Wordles>(where(c(&Wordles::wasUsed) != 1));
 
@@ -98,33 +172,9 @@ int main(void) {
 
     std::string word_of_day = randomWordle[0].word;
 
-    int turns = 0;
-    bool solved = false;
-    while (!solved && turns < 6) {
-        std::string input;
-        std::cout << "Wordle: ";
-        std::cin >> input;
+    std::cout << "==== CPP Wordle (Version 1.0.1) ====\n";
+    std::cout << "\t type q to leave\n";
+    game_loop(word_of_day);
 
-        if (input == "q" || input == "exit") return 0;
-
-        if (wordleInvalid(input)) {
-            std::cout << "That wasn't five letters...\n";
-            continue;
-        }
-        int word_exists = storage.get_all<Wordles>(where(c(&Wordles::word) == input)).size();
-        if (!word_exists) {
-            std::cout << "That word is not in our dictionary\n";
-            continue;
-        }
-        ++turns;
-
-        std::stringstream colouring = getColouring(input, word_of_day);
-        std::cout << colouring.str() << std::endl;
-        if (input == word_of_day) {
-            std::cout << "🎉🎉🎉 in " << turns << " turns!\n";
-            break;
-        }
-    }
-    if (turns == 6) std::cout << "The word was " << word_of_day << std::endl;
     return 0;
 }
